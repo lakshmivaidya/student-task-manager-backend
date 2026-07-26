5ad20207da40d09ae2a8911f3539b5418c0fb07c
unknown <acharlakshmiv66@gmail.com>
2026-07-26 18:18:11 +0530

    Updated_backend

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected Successfully");
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
   })
   .catch((err) => console.log(err));
+
+module.exports = app;
